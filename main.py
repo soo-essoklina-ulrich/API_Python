41c7ffa4bd6f45eeaf933dbb9cb6f89c42b00e45
soo-essoklina-ulrich <essoklina.soo@ipnetinstitute.com>
2023-05-24 20:31:10 +0000

    pushtodeploye3

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 async def root():
- return {"message": "Hello World"}
+ return {"message": "Hello World, SOO ULRICH a deployé son premier API "}
